9797af49d3ee5911fbed9a645ddba00db283719c
Xlaudius <claudio.rivera.sh@gmail.com>
2018-09-09 06:40:57 -0300

    CHG: variable names for readability

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,12 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
-    @done = []
+    @total_done = []
 
     if user_signed_in?
       @done = Checked.select(:user_id, :todo_id).where(:user_id => current_user, :check => true).all
       @done.each do |done_task|
-        @done << done_task.todo_id 
+        @total_done << done_task.todo_id
       end
       how_many_done = @done.length
     end
