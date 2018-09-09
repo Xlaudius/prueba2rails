3ece001781f95773c7c69519099e79bc8527ef6b
Xlaudius <claudio.rivera.sh@gmail.com>
2018-09-09 06:19:59 -0300

    ADD: if 'done' query

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,13 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @done = []
+
+    if user_signed_in?
+      @done = Checked.select(:user_id, :todo_id).where(:user_id => current_user, :check => true).all
+      @done.each do |task|
+        @done << task.todo_id 
+      end
+    end
   end
 end
